5d0306feee38057c7ba8b49a5332378a84e44bda
arpethel <arpethel@gmail.com>
2020-06-14 21:40:34 -0400

    installed connect-mongo, connecting thru mongoDB Atlas with Azure

--- config/session.js
+++ config/session.js
@@ -19,8 +19,10 @@ module.exports.session = {
   *                                                                          *
   ***************************************************************************/
   secret: 'c964d1eaff1c99c0b6e0fbc60e393649',
-
-
+  adapter: 'connect-mongo',
+  url: 'mongodb://testadmin:testadmin111@cluster0-shard-00-00-gr6o0.azure.mongodb.net:27017,cluster0-shard-00-01-gr6o0.azure.mongodb.net:27017,cluster0-shard-00-02-gr6o0.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  //url: 'mongodb://testadmin:testadmin111@ds351428.mlab.com:51428/heroku_5v9p2dtk',
+  //url: 'mongodb://user:pass@host:port/database',
   /***************************************************************************
   *                                                                          *
   * Customize when built-in session support will be skipped.                 *
